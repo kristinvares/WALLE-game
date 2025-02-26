80d2b0de7b6a61452fd5336abab8ce6c31ae9907
Martin Sõna <marson@taltech.ee>
2025-02-26 21:23:00 +0200

    Tegin selle gamedocis 27-40 olevad punktid ära

--- client/core/src/main/java/ee/taltech/WALLE/WALLEGame.java
+++ client/core/src/main/java/ee/taltech/WALLE/WALLEGame.java
@@ -8,30 +8,54 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class WALLEGame extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private Texture opponentImg;
     private int x = 0, y = 0;
+    private Map<Integer, SpriteBatch> opponentBatches = new HashMap<>();
+    private Map<Integer, String> receivedGameObjects = new HashMap<>();
 
     private Client client;
+    private int clientId;
 
     @Override
     public void create() {
+
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        opponentImg = new Texture("drop.jpg");
 
         client = new Client();
         client.start();
-        client.sendTCP("Start");
+        Kryo kryo = client.getKryo();
+        kryo.register(HashMap.class);
         try {
             client.connect(5000, "localhost", 8080, 8081);
+            clientId = client.getID();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        client.addListener(new Listener.ThreadedListener(new Listener() {
+            @Override
+            public void received(Connection connection, Object object) {
+                System.out.print("received: " + object);
+                if (object instanceof Map) {
+                    receivedGameObjects = (Map<Integer, String>) object;
+                }
+            }
+        }));
+
     }
 
     private void sendPositionInfoToServer() {
@@ -46,7 +70,7 @@ public class WALLEGame extends ApplicationAdapter {
             sendPositionInfoToServer();
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            x -= 10;
+            x += 10;
             sendPositionInfoToServer();
         }
         batch.begin();
@@ -54,7 +78,24 @@ public class WALLEGame extends ApplicationAdapter {
         // Agol oli videos nii. Tegin samamoodi
         batch.draw(image, x, y);
         batch.end();
-        client.sendUDP("test");
+        for (Map.Entry<Integer, String> entry : receivedGameObjects.entrySet()) {
+            int id = entry.getKey();
+            if (id == clientId) continue;
+
+            if (!opponentBatches.containsKey(id)) {
+                opponentBatches.put(id, new SpriteBatch());
+            }
+
+            SpriteBatch b = opponentBatches.get(id);
+            String[] coordinates = entry.getValue().split(",");
+            b.begin();
+            b.draw(opponentImg, Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1]));
+            b.end();
+        }
+
+        if (receivedGameObjects != null) {
+            receivedGameObjects.clear();
+        }
     }
 
     @Override
--- server/src/main/java/ee/taltech/game/server/GameServer.java
+++ server/src/main/java/ee/taltech/game/server/GameServer.java
@@ -1,17 +1,25 @@
 package ee.taltech.game.server;
 
+import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GameServer {
     private Server server;
+    private Map<Integer, String> gameObjects = new HashMap<>();
 
     public GameServer() {
         server = new Server();
         server.start();
+        Kryo kryo = server.getKryo();
+        kryo.register(HashMap.class);
+
         try {
             server.bind(8080, 8081);
         } catch (IOException e) {
@@ -23,6 +31,14 @@ public class GameServer {
             // object mida saadeti
             public void received (Connection connection, Object object) {
                 System.out.println(object);
+                if (object instanceof String) {
+                    gameObjects.put(connection.getID(), (String) object);
+                }
+                server.sendToAllUDP(gameObjects);
+            }
+            @Override
+            public void disconnected(Connection connection) {
+                gameObjects.remove(connection.getID());
             }
         });
     }
