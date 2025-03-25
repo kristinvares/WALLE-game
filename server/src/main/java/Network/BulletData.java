fe3c119ea5a423eacfe8e20cff11da0c64ddb7a6
Martin Sõna <marson@taltech.ee>
2025-03-25 12:02:10 +0200

    Tegin tulistamise, kus siis tegin põhikuuli loogika, lisasin serveri ja kliendi vahelise suhtluse ning tegin kuulide eemaldamise mehhanismi

---
+++ server/src/main/java/Network/BulletData.java
@@ -0,0 +1,16 @@
+package Network;
+
+public class BulletData {
+    public int bulletId;
+    public int shooterID;
+    public float x, y;
+    public float directionX, directionY;
+
+    public BulletData() {}
+
+    public BulletData(float x, float y, int shooterID) {
+        this.x = x;
+        this.y = y;
+        this.shooterID = shooterID;
+    }
+}
