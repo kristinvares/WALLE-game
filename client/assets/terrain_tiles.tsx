cbaba50a134fa97a825e779dc35ac7857c9b04b8
Kristin <krvare@taltech.ee>
2025-05-07 23:51:56 +0300

    puuduvad assetid mis algses branchis jäid pushimata

---
+++ client/assets/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terrain_tiles" tilewidth="32" tileheight="32" tilecount="160" columns="10">
+ <image source="simple_tileset_32x32_version1.0/terrain_tiles_v2.png" width="320" height="512"/>
+</tileset>
---
+++ client/assets/water_island_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="water_island_tiles" tilewidth="32" tileheight="32" tilecount="120" columns="15">
+ <image source="simple_tileset_32x32_version1.0/water_and_island_tiles_v2.png" width="480" height="256"/>
+</tileset>
